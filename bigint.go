7c98407a7bf7b9e797d0a6fc5437e94a9cd428da
Bartlomiej Mika <bart@mikasoftware.com>
2024-11-22 15:02:45 -0500

    Added support for `*big.Int`.

---
+++ bigint.go
@@ -0,0 +1,41 @@
+package arraydiff
+
+import (
+	"math/big"
+)
+
+// BigInt Function takes the original array and compare it with the change array to return three arrays: (1) Array with new values added to original array. (2) Array with values not changed between original and change array. (3) Array with values removed from original array.
+func BigInt(originalArr []*big.Int, changeArr []*big.Int) (addedArr, sameArr, removedArr []*big.Int) {
+	var found bool = false
+	// Run the following double-loops to find the items that were removed or
+	// stayed the same.
+	for _, original := range originalArr {
+		for _, change := range changeArr {
+			if original.Cmp(change) == 0 {
+				found = true
+				break
+			}
+		}
+		if found {
+			sameArr = append(sameArr, new(big.Int).Set(original))
+		} else {
+			removedArr = append(removedArr, new(big.Int).Set(original))
+		}
+		found = false
+	}
+
+	// Run the following double-loops to find the items that were added.
+	for _, change := range changeArr {
+		for _, original := range originalArr {
+			if change.Cmp(original) == 0 {
+				found = true
+				break
+			}
+		}
+		if !found {
+			addedArr = append(addedArr, new(big.Int).Set(change))
+		}
+		found = false
+	}
+	return addedArr, sameArr, removedArr
+}
---
+++ bigint_test.go
@@ -0,0 +1,107 @@
+package arraydiff
+
+import (
+	"math/big"
+	"reflect"
+	"testing"
+)
+
+func TestBigIntCase1(t *testing.T) {
+	// Sample data.
+	oldArr := []*big.Int{big.NewInt(1), big.NewInt(2), big.NewInt(3), big.NewInt(4), big.NewInt(5)}
+	newArr := []*big.Int{big.NewInt(1), big.NewInt(2), big.NewInt(6), big.NewInt(7), big.NewInt(8), big.NewInt(9), big.NewInt(10)}
+
+	// Run our test.
+	actualAddedArr, actualSameArr, actualRemovedArr := BigInt(oldArr, newArr)
+
+	// Correct results.
+	expectedAddedArr := []*big.Int{big.NewInt(6), big.NewInt(7), big.NewInt(8), big.NewInt(9), big.NewInt(10)}
+	expectedSameArr := []*big.Int{big.NewInt(1), big.NewInt(2)}
+	expectedRemovedArr := []*big.Int{big.NewInt(3), big.NewInt(4), big.NewInt(5)}
+
+	// Verify.
+	if reflect.DeepEqual(expectedAddedArr, actualAddedArr) == false {
+		t.Errorf("incorrect added array contents, got %v but was expecting %v", actualAddedArr, expectedAddedArr)
+	}
+	if reflect.DeepEqual(expectedSameArr, actualSameArr) == false {
+		t.Errorf("incorrect same array contents, got %v but was expecting %v", actualSameArr, expectedSameArr)
+	}
+	if reflect.DeepEqual(expectedRemovedArr, actualRemovedArr) == false {
+		t.Errorf("incorrect removed array contents, got %v but was expecting %v", actualRemovedArr, expectedRemovedArr)
+	}
+}
+
+func TestBigIntCase2(t *testing.T) {
+	// Sample data.
+	oldArr := []*big.Int{big.NewInt(1), big.NewInt(2), big.NewInt(3), big.NewInt(4), big.NewInt(5)}
+	newArr := []*big.Int{}
+
+	// Run our test.
+	actualAddedArr, actualSameArr, actualRemovedArr := BigInt(oldArr, newArr)
+
+	// Correct results.
+	expectedAddedArr := []*big.Int{}
+	expectedSameArr := []*big.Int{}
+	expectedRemovedArr := []*big.Int{big.NewInt(1), big.NewInt(2), big.NewInt(3), big.NewInt(4), big.NewInt(5)}
+
+	// Verify.
+	if len(actualAddedArr) != 0 {
+		t.Errorf("incorrect added array contents, got %v but was expecting %v", actualAddedArr, expectedAddedArr)
+	}
+	if len(actualSameArr) != 0 {
+		t.Errorf("incorrect same array contents, got %v but was expecting %v", actualSameArr, expectedSameArr)
+	}
+	if reflect.DeepEqual(expectedRemovedArr, actualRemovedArr) == false {
+		t.Errorf("incorrect removed array contents, got %v but was expecting %v", actualRemovedArr, expectedRemovedArr)
+	}
+}
+
+func TestBigIntCase3(t *testing.T) {
+	// Sample data.
+	oldArr := []*big.Int{}
+	newArr := []*big.Int{big.NewInt(1), big.NewInt(2), big.NewInt(3), big.NewInt(4), big.NewInt(5)}
+
+	// Run our test.
+	actualAddedArr, actualSameArr, actualRemovedArr := BigInt(oldArr, newArr)
+
+	// Correct results.
+	expectedAddedArr := []*big.Int{big.NewInt(1), big.NewInt(2), big.NewInt(3), big.NewInt(4), big.NewInt(5)}
+	expectedSameArr := []*big.Int{}
+	expectedRemovedArr := []*big.Int{}
+
+	// Verify.
+	if reflect.DeepEqual(expectedAddedArr, actualAddedArr) == false {
+		t.Errorf("incorrect added array contents, got %v but was expecting %v", actualAddedArr, expectedAddedArr)
+	}
+	if len(actualSameArr) != 0 {
+		t.Errorf("incorrect same array contents, got %v but was expecting %v", actualSameArr, expectedSameArr)
+	}
+	if len(actualRemovedArr) != 0 {
+		t.Errorf("incorrect removed array contents, got %v but was expecting %v", actualRemovedArr, expectedRemovedArr)
+	}
+}
+
+func TestBigIntCase4(t *testing.T) {
+	// Sample data.
+	oldArr := []*big.Int{}
+	newArr := []*big.Int{}
+
+	// Run our test.
+	actualAddedArr, actualSameArr, actualRemovedArr := BigInt(oldArr, newArr)
+
+	// Correct results.
+	expectedAddedArr := []*big.Int{}
+	expectedSameArr := []*big.Int{}
+	expectedRemovedArr := []*big.Int{}
+
+	// Verify.
+	if len(actualAddedArr) != 0 {
+		t.Errorf("incorrect added array contents, got %v but was expecting %v", actualAddedArr, expectedAddedArr)
+	}
+	if len(actualSameArr) != 0 {
+		t.Errorf("incorrect same array contents, got %v but was expecting %v", actualSameArr, expectedSameArr)
+	}
+	if len(actualRemovedArr) != 0 {
+		t.Errorf("incorrect removed array contents, got %v but was expecting %v", actualRemovedArr, expectedRemovedArr)
+	}
+}
