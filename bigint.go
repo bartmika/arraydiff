b48eb88beaa7675cf015300b8a0efc18f5f78814
Bartlomiej Mika <bart@mikasoftware.com>
2024-11-22 22:01:35 -0500

    BUGFIX: Fixed a case with nils for `BigInts`.

--- bigint.go
+++ bigint.go
@@ -4,14 +4,18 @@ import (
 	"math/big"
 )
 
-// BigInt Function takes the original array and compare it with the change array to return three arrays: (1) Array with new values added to original array. (2) Array with values not changed between original and change array. (3) Array with values removed from original array.
+// BigInts Function takes the original array and compare it with the change array to return three arrays: (1) Array with new values added to original array. (2) Array with values not changed between original and change array. (3) Array with values removed from original array.
 func BigInts(originalArr []*big.Int, changeArr []*big.Int) (addedArr, sameArr, removedArr []*big.Int) {
 	var found bool = false
 	// Run the following double-loops to find the items that were removed or
 	// stayed the same.
 	for _, original := range originalArr {
+		if original == nil {
+			continue // skip nil values
+		}
+		found = false
 		for _, change := range changeArr {
-			if original.Cmp(change) == 0 {
+			if change != nil && original.Cmp(change) == 0 {
 				found = true
 				break
 			}
@@ -21,13 +25,16 @@ func BigInts(originalArr []*big.Int, changeArr []*big.Int) (addedArr, sameArr, r
 		} else {
 			removedArr = append(removedArr, new(big.Int).Set(original))
 		}
-		found = false
 	}
 
 	// Run the following double-loops to find the items that were added.
 	for _, change := range changeArr {
+		if change == nil {
+			continue // skip nil values
+		}
+		found = false
 		for _, original := range originalArr {
-			if change.Cmp(original) == 0 {
+			if original != nil && change.Cmp(original) == 0 {
 				found = true
 				break
 			}
@@ -35,7 +42,6 @@ func BigInts(originalArr []*big.Int, changeArr []*big.Int) (addedArr, sameArr, r
 		if !found {
 			addedArr = append(addedArr, new(big.Int).Set(change))
 		}
-		found = false
 	}
 	return addedArr, sameArr, removedArr
 }
